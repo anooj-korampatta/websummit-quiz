3088c7ffd240fa669191e065b5790ff8b577fcad
anooj-korampatta <anoojkorampatta@gmail.com>
2026-01-11 16:18:22 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,25 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
 import {
   getFirestore,
   collection,
   addDoc,
   serverTimestamp,
   enableIndexedDbPersistence
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID"
+  apiKey: "AIzaSyDlrfVa61ePAwS8E4129tyJzTmtEiE0SqM",
+  authDomain: "smart-logistics-quiz.firebaseapp.com",
+  projectId: "smart-logistics-quiz",
+  storageBucket: "smart-logistics-quiz.firebasestorage.app",
+  messagingSenderId: "668279170218",
+  appId: "1:668279170218:web:18967f4f398fc9a70c5cd3"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
-enableIndexedDbPersistence(db);
+// Enable offline support
+enableIndexedDbPersistence(db).catch(() => {});
 
 export { collection, addDoc, serverTimestamp };
